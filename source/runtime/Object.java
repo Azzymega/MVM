798dd2c91e7f96683cc11bc404a93e0466499a41
RedDawn <jdjdjd33loxru@gmail.com>
2023-10-05 16:05:46 +0300

    std update

--- source/runtime/Object.java
+++ source/runtime/Object.java
@@ -6,13 +6,11 @@
  * /
  */
 
-/*
- *
- *  * PROJECT:     MVM - JAVA STDLIB
- *  * LICENSE:     GPL - See COPYING in the top level directory
- *  * PROGRAMMER:  Maltsev Daniil <brickexberiment@lenta.ru>
- * /
- */
+package java.lang;
+
 
 public class Object {
+	public Object(){
+		
+	}
 }
